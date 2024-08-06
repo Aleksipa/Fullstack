58c4c919d527de8e9f94a554ccaef0af66cfc272
Aleksipa <paavola.aleksi@gmail.com>
2024-08-06 16:35:27 +0300

    Add part 9: patientor

---
+++ osa9/patientor-back/src/services/patientService.ts
@@ -0,0 +1,33 @@
+import patients from "../../data/patients";
+import { Patient, NewPatient } from "../types";
+import { NonSensitivePatient } from "../types";
+import { v1 as uuid } from "uuid";
+
+const getPatients = (): Patient[] => {
+  return patients;
+};
+
+const getNonSensitivePatients = (): NonSensitivePatient[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+const addPatient = (patient: NewPatient): Patient => {
+  const newPatient = {
+    id: uuid(),
+    ...patient,
+  };
+  patients.push(newPatient);
+  return newPatient;
+};
+
+export default {
+  getPatients,
+  getNonSensitivePatients,
+  addPatient,
+};
